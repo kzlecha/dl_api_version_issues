d52f95bb449c4b30d6395953b93fb3e929133397
kzlecha <kzlecha@gmail.com>
2020-12-19 00:48:48 -0800

    find differences

--- scripts/analysis/general.py
+++ scripts/analysis/general.py
@@ -1,29 +1,29 @@
 from pandas import read_csv
 
 
-df_all = read_csv("data/unfiltered/all_questions.csv")
+# df_all = read_csv("data/unfiltered/all_questions.csv")
 
-print("All Questions")
-print(df_all.count())
-print(df_all.mean())
-print(df_all.std())
+# print("All Questions")
+# print(df_all.count())
+# print(df_all.mean())
+# print(df_all.std())
 
-df_all = None
+# df_all = None
 
 df_libraries = read_csv("data/filtered/deprecated_libraries.csv")
 df_keywords = read_csv("data/filtered/explicit_mention.csv")
 
 
-print("/nDeprecated Libraries")
-print(df_libraries.count())
-print(df_libraries.mean())
-print(df_libraries.std())
+# print("/nDeprecated Libraries")
+# print(df_libraries.count())
+# print(df_libraries.mean())
+# print(df_libraries.std())
 
 
-print("/nExplicit Mention")
-print(df_keywords.count())
-print(df_keywords.mean())
-print(df_keywords.std())
+# print("/nExplicit Mention")
+# print(df_keywords.count())
+# print(df_keywords.mean())
+# print(df_keywords.std())
 
 
 # split of the libraries
@@ -33,20 +33,36 @@ for i in df_libraries.index:
     row = df_libraries.loc[i]
     for library in libraries.keys():
         # check for questions
-        if library in row["question_body"].lower():
+        if library in row["tags"].lower():
             libraries[library] += 1
-        elif type(row["answer_body"]) == "string" and row["answer_body"] is not None and library in row["answer_body"].lower():
-            libraries[library] += 1
+        # elif library in row["question_body"].lower():
+        #     libraries[library] += 1
+        # elif type(row["answer_body"]) == "string" and row["answer_body"] is not None and library in row["answer_body"].lower():
+        #     libraries[library] += 1
 print(libraries)
 
-libraries = {"pytorch":0, "tensorflow":0, "keras":0, "tesseract":0, "pyspark":0, "caffe":0, "theano":0}
+for library in libraries.keys():
+    print(library)
+    print("count:", libraries[library])
+    print("percentage:", round(libraries[library]/len(df_libraries) * 100, 2), "%")
+
+
+libraries = {"pytorch":0, "tensorflow":0, "keras":0, "tessaract": 0, "tesseract":0, "pyspark":0, "caffe":0, "theano":0}
 
 for i in df_keywords.index:
     row = df_keywords.loc[i]
     for library in libraries.keys():
-        # check for questions
-        if library in row["question_body"].lower():
+        if library in row["tags"].lower():
             libraries[library] += 1
-        elif type(row["answer_body"]) == "string" and row["answer_body"] is not None and library in row["answer_body"].lower():
-            libraries[library] += 1
+        # elif library in row["question_body"].lower():
+        #     libraries[library] += 1
+        # elif type(row["answer_body"]) == "string" and row["answer_body"] is not None and library in row["answer_body"].lower():
+        #     libraries[library] += 1
 print(libraries)
+
+for library in libraries.keys():
+    print(library)
+    print("count:", libraries[library])
+    print("percentage:", round(libraries[library]/len(df_keywords) * 100, 2), "%")
+
+
